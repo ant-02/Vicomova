6f594e66f2d4f496ab53a16498b8b98caa8e4fd0
zll <169343330@qq.com>
2026-05-10 21:47:12 +0800

    cleanup: remove old storage package
    - Delete pkg/infrastructure/storage/ (factory.go, local.go, qiniu.go, storage.go)
    - Replaced by pkg/infrastructure/oss/

--- pkg/infrastructure/storage/storage.go
+++
@@ -1,9 +0,0 @@
-package storage
-
-import "context"
-
-type ImageStorage interface {
-	Upload(ctx context.Context, key string, reader io.Reader) (string, error)
-	Delete(ctx context.Context, key string) error
-	GetURL(ctx context.Context, key string) (string, error)
-}
